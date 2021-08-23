9137d5755414b77eab1e696fb2f34e8eb863fefe
mohdabrar728 <mohdabrar728@gmail.com>
2021-08-23 05:53:43 +0530

    abrar todaty commit

--- HRMS/leave/views.py
+++ HRMS/leave/views.py
@@ -1,16 +1,9 @@
 from django.shortcuts import render
 from .forms import LeaveForm
-from django.views.generic.edit import FormView
+from django.views.generic.edit import CreateView
 
 
-# Create your views here.
-class LeaveView(FormView):
-    template_name = 'leave/leave.html'
-    form_class = LeaveForm
-    success_url = '/thanks/'
-
-    def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.
-        form.send_email()
-        return super().form_valid(form)
+class LeaveView(CreateView):
+ form_class = LeaveForm
+ template_name = 'leave/leave.html'
+ success_url = '/home/'
